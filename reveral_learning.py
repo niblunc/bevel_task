e670b795356623e148062e137f8662e9c87524bb
gracer <gracer@dhcp-v880-11-00060.1Xwireless.unc.edu>
2018-02-06 12:41:53 -0500

    kinda broke it

--- reveral_learning.py
+++ reveral_learning.py
@@ -53,12 +53,10 @@ subdata['SS']={}
 subdata['broke_on_trial']={}
 subdata['simulated_response']=False
 
-subdata['onset']='/Users/'+info['computer']+'/Documents/bevbit_Task/rev_onset_files/onsets_'+info['run']
-subdata['jitter']='/Users/'+info['computer']+'/Documents/bevbit_Task/rev_onset_files/jitter_'+info['run']
-subdata['conds']='/Users/'+info['computer']+'/Documents/bevbit_Task/rev_onset_files/conds_'+info['run']
+subdata['onset']='/Users/'+info['computer']+'/Documents/bevbit_task/rev_onset_files/onsets_'+info['run']
+subdata['jitter']='/Users/'+info['computer']+'/Documents/bevbit_task/rev_onset_files/jitter_'+info['run']
+subdata['conds']='/Users/'+info['computer']+'/Documents/bevbit_task/rev_onset_files/conds_'+info['run']
 subdata['quit_key']='q'
-subdata['1']='left'
-subdata['9']='right'
 
 #######################################
 dataFileName='/Users/'+info['computer']+'/Documents/Output/%s_%s_%s_subdata.log'%(info['participant'],info['session'],subdata['datestamp'])
@@ -84,7 +82,7 @@ time.sleep(1)
 diameter=26.59
 mls_sweet=3.0
 mls_unsweet=3.0
-mls_H2O=3.0
+#mls_H2O=3.0
 mls_rinse=1.0
 delivery_time=6.0
 cue_time=2.0
@@ -95,12 +93,12 @@ ans_time=2.0
 str='\r'
 rate_sweet = mls_sweet*(3600.0/delivery_time)  # mls/hour 300
 rate_unsweet = mls_unsweet*(3600.0/delivery_time)  # mls/hour 300
-rate_H2O = mls_H2O*(3600.0/delivery_time)  # mls/hour 300
+#rate_H2O = mls_H2O*(3600.0/delivery_time)  # mls/hour 300
 rate_rinse = mls_rinse*(3600.0/rinse_time)  # mls/hour 300
+
 #pump set up stuff NEED TO CLEAN UP
 pump_setup = ['0VOL ML\r', '1VOL ML\r', '2VOL ML\r']
-pump_phases=['0PHN01\r','1PHN01\r', '2PHN01\r','0CLDINF\r','1CLDINF\r','2CLDINF\r','0DIRINF\r','1DIRINF\r','2DIRINF\r','0RAT%iMH\r'%rate_H2O,'1RAT%iMH\r'%rate_sweet,'2RAT%iMH\r'%rate_unsweet,'0VOL%i%s'%(mls_H2O,str), '1VOL%i%s'%(mls_sweet,str),'2VOL%i%s'%(mls_unsweet,str),'0DIA%.2fMH\r'%diameter,'1DIA%.2fMH\r'%diameter, '2DIA%.2fMH\r'%diameter]
-pump_phases2=['0PHN02\r','0CLDINF\r','0DIRINF\r','0RAT%iMH\r'%rate_rinse,'0VOL%i%s'%(mls_rinse,str), '0DIA%.2fMH\r'%diameter]
+pump_phases=['0PHN01\r','1PHN01\r', '2PHN01\r','0CLDINF\r','1CLDINF\r','2CLDINF\r','0DIRINF\r','1DIRINF\r','2DIRINF\r','0RAT%iMH\r'%rate_rinse,'1RAT%iMH\r'%rate_sweet,'2RAT%iMH\r'%rate_unsweet,'0VOL%i%s'%(mls_rinse,str), '1VOL%i%s'%(mls_sweet,str),'2VOL%i%s'%(mls_unsweet,str),'0DIA%.2fMH\r'%diameter,'1DIA%.2fMH\r'%diameter, '2DIA%.2fMH\r'%diameter]
 
 
 
@@ -136,6 +134,8 @@ def check_for_quit(subdata,win):
     else:
         return False
 
+
+
 def tastes(params):
     for c in params:
         ser.write(c)
@@ -166,7 +166,7 @@ f=open(subdata['onset'],'r')
 x = f.readlines()
 for i in x:
     onsets.append(i.strip())
-
+print(onsets)
 onsets=[float(i) for i in onsets]
 print(onsets, 'onsets')
 
@@ -222,11 +222,13 @@ def run_block():
     
 
     ratings_and_onsets.append(['fixation',t])
+    logging.log(logging.DATA, "fixation %f"%t)
     show_stim(fixation_text, 8)  # 8 sec blank screen with fixation cross
+    logging.log(logging.DATA, "fixation end %f"%t)
     t = clock.getTime()
     clock.reset()
     ratings_and_onsets.append(['start',t])
-    logging.log(logging.DATA, "start")
+    logging.log(logging.DATA, "START")
     for trial in range(ntrials):
         if check_for_quit(subdata,win):
             exptutils.shut_down_cleanly(subdata,win)
@@ -264,19 +266,22 @@ def run_block():
         message.draw()
         #this is logging when the message is shown
         logging.log(logging.DATA, "visual_stim1=%s at position=%s"%(stim_images[indices[0]],positions_eng[pos_ind[0]]))
-        
 
         while clock.getTime()<trialdata['onset']:
             pass
         win.flip()
             
-        while clock.getTime()<(trialdata['onset']+cue_time):#show the image
+        while clock.getTime()<(trialdata['onset']+cue_time):#show the image, while clock is less than onset and cue, show cue
             pass
             
-
+        message=visual.TextStim(win, text='')#blank screen while the taste is delivered
+        message.draw()
+        win.flip()
+        
+        
         # get the key press logged, and time stamped 
         keys = event.getKeys(timeStamped=clock)
-        logging.log(logging.DATA, "keypress=%s at time= %f"%(keys[0][0],keys[0][1])
+        logging.log(logging.DATA, "keypress=%s at time= %f"%(keys[0][0],keys[0][1]))
         print("here are the keys:")
         print(keys)
         
@@ -288,8 +293,6 @@ def run_block():
         
         while clock.getTime()<(trialdata['onset']+cue_time):#show the image
             pass
-#        print('test')
-#        print(keys[0][0])
         
         if keys[0][0] == 'left':
             #from the dictionary find the pump code associated with the key press
@@ -301,6 +304,7 @@ def run_block():
             ratings_and_onsets.append(["injecting via pump at address %d"%taste, t, keys[0][0]])
             #trigger pump with the numeral from the dictonary above 
             ser.write('%dRUN\r'%taste)
+            
             
         if keys[0][0] == 'right':
             #from the dictonary get the pump associated with the right key press
@@ -314,6 +318,7 @@ def run_block():
             #trigger the pump with the numeral from the dictionary
             ser.write('%dRUN\r'%taste)
             
+                
         while clock.getTime()<(trialdata['onset']+cue_time+delivery_time):
             pass
         
@@ -322,6 +327,8 @@ def run_block():
         win.flip()
         t = clock.getTime()
         ratings_and_onsets.append(["wait", t])
+        
+        
         
         trialdata['dis']=[ser.write('0DIS\r'),ser.write('1DIS\r')]
         print(trialdata['dis'])
@@ -338,7 +345,9 @@ def run_block():
         ratings_and_onsets.append(['injecting rinse via pump at address %d'%0, t])
         ser.write('%dRUN\r'%0)
         
-        while clock.getTime()<(trialdata['onset']+cue_time+ans_time+delivery_time+wait_time+rinse_time):
+      
+        
+        while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time+rinse_time):
             pass
 
         message=visual.TextStim(win, text='+', pos=(0, 0), height=2)#lasts through the jitter 
@@ -347,7 +356,7 @@ def run_block():
         t = clock.getTime()
         ratings_and_onsets.append(["jitter", t])
 
-        while clock.getTime()<(trialdata['onset']+cue_time+ans_time+delivery_time+wait_time+rinse_time+jitter[trial]):
+        while clock.getTime()<(trialdata['onset']+cue_time+delivery_time+wait_time+rinse_time+jitter[trial]):
             pass
         
         t = clock.getTime()
@@ -355,6 +364,9 @@ def run_block():
         logging.log(logging.DATA,"finished")
         subdata['trialdata'][trial]=trialdata
         
+      
+        
+        print(key_responses)
     win.close()
 
 
